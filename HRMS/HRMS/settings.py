0e0c82e62f597859d2ba39ddba1687d22c977938
ARJUN T <83713729+arjunt-dev@users.noreply.github.com>
2025-11-26 19:33:29 +0530

    Change SECRET_KEY to use environment variable
    Updated SECRET_KEY to use environment variable.

--- HRMS/HRMS/settings.py
+++ HRMS/HRMS/settings.py
@@ -24,7 +24,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-yio+emtk=o3!d(m7xjqo8hj#&4%-9!+1@x@mx2!ma13$c=%&()"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
